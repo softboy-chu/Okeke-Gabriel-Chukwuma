4151a00ade4b042f6966bc3ec13ea79b0439d7ae
Okeke Gabriel Chukwuma <okekechukwuma88@gmail.com>
2024-02-04 15:13:23 -0800

    Add files via upload

--- about.js
+++ about.js
@@ -1,16 +1,24 @@
 const sideBar = document.getElementById('sideBar')
 sideBar.style.display = 'none'
-              
+
 const navBar = document.getElementById('navBar')
 const body = document.getElementById('body')
 navBar.addEventListener('click', function(){
-    navBar.style.display = 'none'
-    sideBar.style.display = 'block';
+  navBar.style.display = 'none'
+  sideBar.style.display = 'block';
 })
-              
-let navBarExit = document.getElementById('navBarExit');
-              
-  navBarExit.addEventListener('click', function(){
+
+let navBarExit = document.getElementById('navBarExit')
+
+navBarExit.addEventListener('click', function(){
   sideBar.style.display = 'none'
-  });
-              
+  navBar.style.display = 'block'
+}
+) 
+
+let container = document.querySelector(`body`)
+if(sideBar.style.display === 'block'){
+  container.addEventListener('click', function(){
+    sideBar.style.display = 'none'
+  })
+}             
